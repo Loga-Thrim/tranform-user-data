85dded030e6b09aef1c10cfc6166eaa3fd7e1ee8
Surachet Yongyut <sueanoy@Sueanoy-2.local>
2024-02-25 00:54:07 +0700

    update constant variable

--- app.ts
+++ app.ts
@@ -1,18 +1,19 @@
 import express from "express";
 import { IUserResponse } from "./models";
 import { transformUserData } from "./services/user";
+import { PORT, USER_API } from "./constants/common";
 
 const app = express();
 
 app
   .get("/get-user", async (req, res) => {
-    const fetchUsers = await fetch("https://dummyjson.com/users");
+    const fetchUsers = await fetch(USER_API);
     const { users } = (await fetchUsers.json()) as IUserResponse;
 
     const newUsers = transformUserData(users);
 
     res.json(newUsers);
   })
-  .listen(5500, () => {
+  .listen(PORT, () => {
     console.log("> App on port 5500.");
   });
---
+++ constants/common.ts
@@ -0,0 +1,2 @@
+export const PORT = 5500;
+export const USER_API = 'https://dummyjson.com/users';
